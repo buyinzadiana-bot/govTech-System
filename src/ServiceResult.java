e73a8a6d9f842bfd997dcb08d2ce982cef27cd80
Diana BUYINZA <buyinzadiana@gmail.com>
2026-02-12 16:37:04 +0200

    .

---
+++ src/ServiceResult.java
@@ -0,0 +1,21 @@
+public class ServiceResult {
+
+    private String message;
+
+    public ServiceResult(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
